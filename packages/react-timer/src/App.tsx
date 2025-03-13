4618b9e659eeaf1bbf43226adb99f214c423bdf0
Rob Park <rbbpark@gmail.com>
2025-03-12 20:19:01 -0700

    expose component API with useImperativeHandle on Form

--- packages/react-timer/src/App.tsx
+++ packages/react-timer/src/App.tsx
@@ -1,43 +1,39 @@
-import { useRef } from "react";
-import List from "./components/List";
+import React, { useRef } from "react";
+import Button from "./components/Button";
+import Form, { type FormHandle } from "./components/Form";
 import Input from "./components/Input";
 
 function App() {
-  type User = {
-    id: string;
-    name: string;
-  };
+  const customForm = useRef<FormHandle>(null);
 
-  const users: User[] = [
-    { id: "1", name: "Ted" },
-    { id: "2", name: "Billy" },
-  ];
-
-  const hobbies = ["golf", "tennis", "kickboxing"];
-
-  const inputRef = useRef<HTMLInputElement>(null);
+  function handleSave(data: unknown) {
+    const extractedData = data as { name: string; age: string };
+    console.log(extractedData);
+    customForm.current?.clear();
+  }
 
   return (
     <main>
-      <Input
-        label="Test"
-        id="test"
-        ref={inputRef}
-      />
-      <section>
-        <h2>Users</h2>
-        <List
-          items={users}
-          renderItem={(user) => <li key={user.id}>{user.name}</li>}
+      <Form
+        onSave={handleSave}
+        ref={customForm}
+      >
+        <Input
+          type="text"
+          label="Name"
+          id="name"
+          name="name"
         />
-      </section>
-      <section>
-        <h2>Hobbies</h2>
-        <List
-          items={hobbies}
-          renderItem={(hobby) => <li key={hobby}>{hobby}</li>}
+        <Input
+          type="number"
+          label="Age"
+          id="age"
+          name="age"
         />
-      </section>
+        <p>
+          <Button>Save</Button>
+        </p>
+      </Form>
     </main>
   );
 }
---
+++ packages/react-timer/src/components/Form.tsx
@@ -0,0 +1,42 @@
+import React, { forwardRef, useImperativeHandle, useRef } from "react";
+
+export type FormHandle = {
+  clear: () => void;
+};
+
+type Props = {
+  children: React.ReactNode;
+  onSave: (value: unknown) => void;
+} & React.ComponentPropsWithoutRef<"form">;
+
+const Form = forwardRef<FormHandle, Props>(function Form({ onSave, children, ...props }, ref) {
+  const form = useRef<HTMLFormElement>(null);
+
+  useImperativeHandle(ref, () => {
+    return {
+      clear() {
+        console.log("clear");
+        form.current?.reset();
+      },
+    };
+  });
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const data = Object.fromEntries(formData);
+    onSave(data);
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      ref={form}
+      {...props}
+    >
+      {children}
+    </form>
+  );
+});
+
+export default Form;
