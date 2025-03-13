8dd96a650f643d0fca8a97ddfa31522c31280641
Rob Park <rbbpark@gmail.com>
2025-03-12 23:05:05 -0700

    update react-timer to new starting code

--- packages/react-timer/src/App.tsx
+++ packages/react-timer/src/App.tsx
@@ -1,37 +1,16 @@
-import { useRef } from "react";
-import { Button, Form, Input, type FormHandle } from "@udemy-ts-react/shared";
+import AddTimer from './components/AddTimer.tsx';
+import Header from './components/Header.tsx';
+import Timers from './components/Timers.tsx';
+
 function App() {
-  const customForm = useRef<FormHandle>(null);
-
-  function handleSave(data: unknown) {
-    const extractedData = data as { name: string; age: string };
-    console.log(extractedData);
-    customForm.current?.clear();
-  }
-
   return (
-    <main>
-      <Form
-        onSave={handleSave}
-        ref={customForm}
-      >
-        <Input
-          type="text"
-          label="Name"
-          id="name"
-          name="name"
-        />
-        <Input
-          type="number"
-          label="Age"
-          id="age"
-          name="age"
-        />
-        <p>
-          <Button>Save</Button>
-        </p>
-      </Form>
-    </main>
+    <>
+      <Header />
+      <main>
+        <AddTimer />
+        <Timers />
+      </main>
+    </>
   );
 }
 
